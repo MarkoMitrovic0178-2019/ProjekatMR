ca0319bc98c75a406f0ba6b527b549a34b90640a
Marko Mitrovic <mitrovicmarko327743@gmail.com>
2023-10-07 17:22:32 +0200

    Added a user class and movie class, and in moviesService i added crud methods for movies, and made it so every user can change and see only their list of movies.

---
+++ src/app/movies/movies.model.ts
@@ -0,0 +1,5 @@
+export class Movie{
+    constructor(public id:string,public director:string,public name:string,public imageUrl:string,public critique:string,
+                public userId:string) {
+    }
+}
